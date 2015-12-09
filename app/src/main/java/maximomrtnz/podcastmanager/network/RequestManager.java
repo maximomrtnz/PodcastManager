4035fb1bb0e30db1b023c9703fc71b1fe4143f92
Maximo Martinez Gonzalez <maximomrtnz@gmail.com>
2015-12-08 22:40:52 -0300

    RequestManager added
    RequestManager is a singleton class to improve volley usage.

---
+++ app/src/main/java/maximomrtnz/podcastmanager/network/RequestManager.java
@@ -0,0 +1,53 @@
+package maximomrtnz.podcastmanager.network;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Maxi on 12/8/2015.
+ */
+public class RequestManager {
+
+    private static RequestManager instance;
+    private RequestQueue mRequestQueue;
+    private static Context mContext;
+
+    private RequestManager(Context context) {
+        mContext = context;
+        mRequestQueue = getRequestQueue();
+    }
+
+
+    // This method should be called first to do singleton initialization
+    public static synchronized RequestManager getInstance(Context context) {
+        if (instance == null) {
+            instance = new RequestManager(context);
+        }
+        return instance;
+    }
+
+    public static synchronized RequestManager getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException(RequestManager.class.getSimpleName() +
+                    " is not initialized, call getInstance(..) method first.");
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+    
+}
